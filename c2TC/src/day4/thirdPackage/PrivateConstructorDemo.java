c4bf756dcf4595c5232f5155950f2dc9729185cd
bharani <bharani@localhost>
2025-08-19 07:25:14 +0530

    day4 completed

---
+++ c2TC/src/day4/thirdPackage/PrivateConstructorDemo.java
@@ -0,0 +1,19 @@
+package day4.thirdPackage;
+
+public class PrivateConstructorDemo {
+	public static void main(String s[]) {
+		// Can't create object with private constructor
+		// MyClass m1=new MyClass();
+		
+		Myclass m=Myclass.getObject();
+		m.setId(10);
+		
+		Myclass m1=Myclass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
+}
+
+
